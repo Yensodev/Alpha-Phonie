a7f300818913d5fb690dd19364eb3cc3a8cf2bad
Onyenso <yensoroutebird@gmail.com>
2022-07-27 00:36:20 +0100

    fixed theme-switch display issue

--- app.mjs
+++ app.mjs
@@ -1,26 +1,38 @@
 networks = {
-	airtel: [
-		"0701", "0708", "0802", "0808",
-		"0812", "0901", "0902", "0904",
-		"0907", "0912"
-	],
+	airtel: {
+		numbers: [
+			"0701", "0708", "0802", "0808",
+			"0812", "0901", "0902", "0904",
+			"0907", "0912"
+		],
+		logo: "url('./pictures/airtel logo.jpg')"
+	},
 
-	mtn: [
-		"0703", "0704", "0706", "0803",
-		"0806", "0810", "0813", "0814",
-		"0816", "0903", "0906", "0913",
-		"0916",
-	],
+	mtn: {
+		numbers: [
+			"0703", "0704", "0706", "0803",
+			"0806", "0810", "0813", "0814",
+			"0816", "0903", "0906", "0913",
+			"0916",
+		],
+		logo: "url('./pictures/mtn logo.png')"
+	},
 
-	glo: [
-		"0705", "0805", "0807", "0811",
-		"0815", "0905", "0915",
-	],
+	glo: {
+		numbers: [
+			"0705", "0805", "0807", "0811",
+			"0815", "0905", "0915",
+		],
+		logo: "url('./pictures/Globacom Limited Logo.png')"
+	},
 
-	etisalat: [
-		"0809", "0817", "0818", "0909",
-		"0908",
-	]
+	etisalat: {
+		numbers: [
+			"0809", "0817", "0818", "0909",
+			"0908",
+		],
+		logo: "url('./pictures/9mobile logo.jpg')"
+	}
 }
 
 
@@ -29,7 +41,6 @@ function startApp() {
 
 	darkMode = true
 	theme = document.querySelector("#theme")
-	theme.href = darkMode ? "bootstrap.min.css" : "bootstrap-light.min.css"
 	inputTags = document.querySelectorAll(".input")
 	inputTags.forEach(element => element.oninput = checkAndChange)
 
@@ -47,28 +58,13 @@ function checkAndChange(e) {
 	
 	for (network in networks) {
 
-		includes = networks[network].includes(value.slice(0, 4))
+		includes = networks[network]["numbers"].includes(value.slice(0, 4))
 
 		if (includes && value !== "" && !isNaN(value)) {
-
-			if (network == "airtel") {
-				e.target.style.backgroundImage = "url('./pictures/airtel logo.jpg')"
-				break
-			}
-			else if (network == "mtn") {
-				e.target.style.backgroundImage = "url('./pictures/mtn logo.png')"
-				break
-			}
-			else if (network == "glo") {
-				e.target.style.backgroundImage = "url('./pictures/Globacom Limited Logo.png')"
-				break
-			}
-			else if (network == "etisalat") {
-				e.target.style.backgroundImage = "url('./pictures/9mobile logo.jpg')"
-				break
-			}
+			console.log(network)
+			e.target.style.backgroundImage = networks[network]["logo"]
+			break
 		}
-
 		else e.target.style.backgroundImage = "none";
 	}
 }
